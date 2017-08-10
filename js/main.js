e23fa20d567504d901ec242bc41e98219c5732b2
milagrosMndoza <milagrosmndoza@gmail.com>
2017-08-10 15:48:20 -0500

    Implementando mi boton X y comentando mi proyecto

--- js/main.js
+++ js/main.js
@@ -2,50 +2,57 @@
 var mInput = document.getElementById("primerInput");
 var contenedor = document.getElementById("contenedor-lista");
 var btnGuardar = document.getElementById('boton-guardar');
-
-
+var btnX = document.getElementById('btnX');
+//Creo una funcion par llamar a mi boton guardar
 mInput.addEventListener("click", function(){
-    
-    // Alternar entre agregar y quitar un nombre de clase de un elemento con JavaScript.
-    btnGuardar.classList.toggle("show");
+/*La propiedad classList devuelve el nombre de la clase de un elemento.Esta propiedad es útil para agregar, quitar y alternar clases CSS en un elemento.Puede modificarla utilizando los métodos add () y remove ().*/
+//toggle:Alternar entre agregar y quitar un nombre de clase de un elemento con JavaScript.
+btnGuardar.classList.toggle("show");
+btnX.classList.toggle("show2");
 });
 
 btnGuardar.addEventListener("click", guardarLista);
 
-//Funcion que crea Lista despues de ser clickeado
-
+//Función que crea Lista despues de ser clickeado
 function guardarLista(){
-	var mInput = document.getElementById("primerInput").value;
+	mInput = document.getElementById("primerInput").value;
 	document.getElementById('primerInput').value = "";
+    
+     //validacion que input no este vacio
+       
     //El método querySelector devuelve el primer elemento que coincide con un selector CSS especificado en el documento.
 	var espacioLista = document.querySelector(".nueva-lista");
+    //Creamos nuestro primer div
 	var nuevoInputLista = document.createElement("div");
-    /*La propiedad classList devuelve el nombre de la clase de un elemento, como un objeto DOMTokenList.Esta propiedad es útil para agregar, quitar y alternar clases CSS en un elemento.
-    puede modificarla utilizando los métodos add () y remove ().*/
+    //A mi nuevo input le pongo classList.add y le doy un id "Lista-contenido" para que mi nueva tarea me aparezca al costado
 	nuevoInputLista.classList.add("lista-contenido");
-	//creamos un div
+	//Creamos nuestro 2do div
     var divHeader = document.createElement("div");
 	divHeader.classList.add("lista-header");
 	var listaWrite = document.createElement("h4");
+    
+    //Utilizo la propiedad "contenteditable" para poder editar lo que escribo en mi lista.
     listaWrite.setAttribute('contenteditable', 'true');
 	var h4Text = document.createTextNode(mInput);
-	var divEnlace = document.createElement("div");
-	var enlaceAgregarTarjeta = document.createElement("a");
-	var textoAgregarTarjeta = document.createTextNode("Añadir una tarjeta...");
-    
-    espacioLista.appendChild(nuevoInputLista);
-	nuevoInputLista.appendChild(divHeader);
-	divHeader.appendChild(listaWrite);
-	listaWrite.appendChild(h4Text);
-	nuevoInputLista.appendChild(divEnlace);
-	divEnlace.appendChild(enlaceAgregarTarjeta);
-	enlaceAgregarTarjeta.appendChild(textoAgregarTarjeta);
-
-	enlaceAgregarTarjeta.addEventListener('click', function(){
+        //creamos el 3er div para AgregarTarjeta
+        var divEnlace = document.createElement("div");
+        var enlaceAgregarTarjeta = document.createElement("a");
+        var textoAgregarTarjeta = document.createTextNode("Añadir una tarjeta...");
+        /* var botonAnadir = document.createElement("button");
+        botonAnadir.setAttribute("type", "submit");
+        botonAnadir.appendChild(textoAgregarTarjeta);*/
+        espacioLista.appendChild(nuevoInputLista);
+        nuevoInputLista.appendChild(divHeader);
+        divHeader.appendChild(listaWrite);
+        listaWrite.appendChild(h4Text);
+        nuevoInputLista.appendChild(divEnlace);
+        divEnlace.appendChild(enlaceAgregarTarjeta);
+	    enlaceAgregarTarjeta.appendChild(textoAgregarTarjeta);
+	    enlaceAgregarTarjeta.addEventListener('click', function(){
+        //Creamos otro div para nuestra caja de texto
 		var divCajaTexto = document.createElement("div");
 	   	var cajaTexto = document.createElement('textarea');
-			
-		nuevoInputLista.appendChild(divCajaTexto);
+        nuevoInputLista.appendChild(divCajaTexto);
 		divCajaTexto.appendChild(cajaTexto);
 		divCajaTexto.appendChild(divEnlace);
     });
